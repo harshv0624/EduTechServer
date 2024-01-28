e75d597131bccf7f6e49c4c5155bfc28bf178ee8
harshv0624 <harshvonmail@gmail.com>
2024-01-29 03:00:25 +0530

    Backend

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const body=require('body-parser')
 const jwt=require('jsonwebtoken')
 const cors=require('cors')
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+} 
 const app=express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(body.json())
 require('dotenv').config()
 require('./Models/USER')
